4cf828da14346bda1ae6fa25950d799ffd99a3b4
kaesar <kaesar.andres@gmail.com>
2025-03-09 09:54:43 -0500

    v0.3.0 Rust + BoaJS release

--- src/main.rs
+++ src/main.rs
@@ -1,52 +1,18 @@
+use std::fs;
+use std::io::Write;
 use clap::{Arg, Command};
 use rust_embed::RustEmbed;
 use boa_engine::{Context, Source, JsValue, JsString};
-use std::fs;
-use std::io::Write;
 
 #[derive(RustEmbed)]
 #[folder = "abcodejs/"]
 struct Asset;
 
-const PRE_HEADER: &str = r#"// © 2021-2025 by César Andres Arcila Buitrago
-
-const console = {
-    messages: [],
-    log: function(message) {
-        this.messages.push("LOG: " + message);
-    },
-    warn: function(message) {
-        this.messages.push("WARN: " + message);
-    },
-    error: function(message) {
-        this.messages.push("ERROR: " + message);
-    }
-}
-
-function getConsoleMessages() {
-    return console.messages.join('\n');
-}
-
-String.prototype.substr = function(start, length) {
-    if (start < 0) {
-        start = this.length + start;
-        if (start < 0) start = 0;
-    }
-    if (length === undefined) {
-        return this.substring(start);
-    }
-    if (length < 0) {
-        return '';
-    }
-    return this.substring(start, start + length);
-}
-"#;
-
 fn main() {
-    // Configuración de Clap para parsear los argumentos de línea de comandos
+    // Setting Clap for command line arguments
     let matches = Command::new("abcodec")
-        .version("1.0")
-        .about("ABCode transpiler")
+        .version("0.3.0")
+        .about("ABCode Compiler (Transpiler)")
         .arg(
             Arg::new("target")
                 .short('t')
@@ -69,7 +35,7 @@ fn main() {
     let plan = "*";
     let output = "./run/";
 
-    // Mensaje inicial
+    // Initial message
     println!(
         "\nINIT => target: {} | script: {} | plan: {} | output: {} | os: {}\n",
         target,
@@ -79,7 +45,7 @@ fn main() {
         std::env::consts::OS
     );
 
-    // Lógica principal: si target > 6, muestra los targets; si no, procesa el script
+    // Main logic: In case of target > 6, it shows the target options
     if target > 6 {
         println!("Target language or runtime: 1. Node.js, 2. Deno, 3. Wasm, 4. Python, 5. Lua, 6. Kotlin");
     } else {
@@ -90,7 +56,7 @@ fn main() {
 }
 
 fn get_new_file(target: i32, script_file: &str) -> String {
-    // Determina la extensión según el target
+    // Set extension according to the target
     let extension = match target {
         1 => ".js",    // Node.js
         2 => ".ts",    // Deno
@@ -100,28 +66,45 @@ fn get_new_file(target: i32, script_file: &str) -> String {
         6 => ".kt",    // Kotlin
         _ => ".js",    // Por defecto
     };
-    // Reemplaza "abc" por "run" y cambia la extensión
+
+    // Replace first "abc" with "run"
     let newfile = if let Some(pos) = script_file.find("abc") {
         let (before, after) = script_file.split_at(pos);
         format!("{}run{}", before, &after[3..])
     } else {
         script_file.to_string()
     };
+    
     newfile.replace(".abc", extension)
 }
 
+macro_rules! get_console_messages {
+    ($context:expr) => {{
+        let console_messages_key = JsString::from("getConsole");
+        let console_messages_fn = $context
+            .global_object()
+            .get(console_messages_key, &mut $context)
+            .unwrap();
+        let console_messages_fn = console_messages_fn
+            .as_callable()
+            .unwrap();
+        let console_messages_result = console_messages_fn
+            .call(&JsValue::Undefined, &[], &mut $context)
+            .unwrap();
+        console_messages_result.as_string().unwrap().to_std_string().unwrap()
+    }};
+}
+
 fn get_plain_js(target: i32, script_file: &str, plan: &str) {
-    // Crea el directorio de salida si no existe
     fs::create_dir_all("./run").unwrap();
 
-    // Calcula el nombre del archivo de salida
     let newfile = get_new_file(target, script_file);
-
-    // Carga el encabezado desde los archivos embebidos
+    let pre_header_bytes = Asset::get("abchelper.js").unwrap().data;
+    let pre_header_code = String::from_utf8(pre_header_bytes.to_vec()).unwrap();
     let header_bytes = Asset::get("abcode.js").unwrap().data;
     let header_code = String::from_utf8(header_bytes.to_vec()).unwrap();
 
-    // Carga el transpilador según el target
+    // Load the transpiler file according to the target
     let transpiler_file = match target {
         1 => "node.js",
         2 => "deno.js",
@@ -131,26 +114,26 @@ fn get_plain_js(target: i32, script_file: &str, plan: &str) {
         6 => "kotlin.js",
         _ => "node.js",
     };
-    let transpiler_bytes = Asset::get(transpiler_file).unwrap().data;
+    let transpiler_bytes = Asset::get(transpiler_file).unwrap_or_else(|| {
+        panic!("ERROR: Language compiler/transpiler not found for target {}", target);
+    }).data;
     let transpiler_code = String::from_utf8(transpiler_bytes.to_vec()).unwrap();
 
-    // Lee el script ABCode desde el archivo
+    // Read the script file and combine it with the pre-header and header
     let script_code = fs::read_to_string(script_file).unwrap();
+    let compiler = format!("{}{}{}", pre_header_code, header_code, transpiler_code);
+    fs::write("./run/abcodec.js", &compiler).unwrap();  // Save the compiler for debugging
 
-    // Combina el encabezado y el transpilador
-    let compiler = format!("{}\n{}{}", PRE_HEADER, header_code, transpiler_code);
-    fs::write("./run/abcodec.js", &compiler).unwrap(); // Guarda el compilador (quizás para depuración)
-
-    // Mensaje de compilación
+    // Message for the user
     print!("Compiling... {}", script_file);
     std::io::stdout().flush().unwrap();
 
-    // Crea un contexto de Boa y evalúa el compilador
+    // Create Boa context and evaluate the compiler/transpiler
     let mut context = Context::default();
     let script = Source::from_bytes(compiler.as_bytes());
     context.eval(script).unwrap();
 
-    // Obtiene la función "start" y la llama con los argumentos
+    // Get the "start" function from Boa context
     let key = JsString::from("start");
     let binding = context
         .global_object()
@@ -166,26 +149,15 @@ fn get_plain_js(target: i32, script_file: &str, plan: &str) {
 
     let result = start_fn.call(&JsValue::Undefined, &args, &mut context).unwrap();
     let code = result.as_string().unwrap().to_std_string().unwrap();
+    let console_messages = get_console_messages!(context);
 
-    // Obtiene los mensajes de consola
-    let console_messages_key = JsString::from("getConsoleMessages");
-    let console_messages_fn = context
-        .global_object()
-        .get(console_messages_key, &mut context)
-        .unwrap();
-    let console_messages_fn = console_messages_fn
-        .as_callable()
-        .unwrap();
-    let console_messages_result = console_messages_fn.call(&JsValue::Undefined, &[], &mut context).unwrap();
-    let console_messages = console_messages_result.as_string().unwrap().to_std_string().unwrap();
-
-    // Muestra el resultado y lo guarda
+    // Show the result and save it in the new file
     println!(" Ok!\nGenerating... {}", newfile);
-    println!("---\n{:?} \n", code);
-    println!("---\n{}", console_messages);
+    println!("---\n{} \n", console_messages);
+    println!("---\n{}", code);
     fs::write(&newfile, &code).unwrap();
 
-    // Genera el script de compilación/ejecución
+    // Generate the shell script for compiling the new file
     compile_target(target, &newfile);
 }
 
@@ -219,7 +191,7 @@ fn compile_target(target: i32, file: &str) {
         _ => return,
     };
 
-    // Guarda el script en ./run/abctest.sh
+    // Save the shell script
     let shell = "./run/abctest.sh";
     fs::write(shell, compiler).unwrap();
 }
